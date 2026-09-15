15a7fe3631dfe3252eb7e72f5875a6adb987422b
DevGon <ghcheondev@gmail.com>
2026-09-15 23:51:45 +0900

    feat: add startup option to desktop settings

--- packages/desktop/src/preload.cjs
+++ packages/desktop/src/preload.cjs
@@ -3,6 +3,7 @@ contextBridge.exposeInMainWorld('jjinmak', {
   getState: () => ipcRenderer.invoke('get-state'),
   setArmed: (enabled) => ipcRenderer.invoke('set-armed', enabled),
   setOptions: (options) => ipcRenderer.invoke('set-options', options),
+  setStartup: (enabled) => ipcRenderer.invoke('set-startup', enabled),
   cancelShutdown: () => ipcRenderer.invoke('cancel-shutdown'),
   demoEvent: (kind) => ipcRenderer.invoke('demo-event', kind),
   onState: (callback) => {
